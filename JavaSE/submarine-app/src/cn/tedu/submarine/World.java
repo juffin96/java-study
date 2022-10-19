c9418ee25c4309713850c1d8a87609cdf6b509dd
juffin96 <juffin96@gmail.com>
2022-10-19 11:47:08 +0800

    潜艇大战项目

--- JavaSE/submarine-app/src/cn/tedu/submarine/World.java
+++ JavaSE/submarine-app/src/cn/tedu/submarine/World.java
@@ -3,6 +3,8 @@ package cn.tedu.submarine;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 游戏窗口
@@ -10,22 +12,13 @@ import java.awt.Graphics;
 public class World extends JPanel {
     public static final int WIDTH = 641;
     public static final int HEIGHT = 479;
-
     //如下就是窗口中所看到的对象
-    private Battleship ship = new Battleship();
-    private SeaObject[] submarines = {
-            new ObserveSubmarine(),
-            new MineSubmarine(),
-            new TorpedoSubmarine()
-    };
-    private Mine[] mines = {
-            new Mine(200, 300)
-    };
-    private Bomb[] bombs = {
-            new Bomb(300, 400)
-    };
+    private static Battleship ship = new Battleship();
+    private static SeaObject[] submarines = {};
+    private static Mine[] mines = {};
+    private static Bomb[] bombs = {};
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         //画海洋
         Images.sea.paintIcon(null, g, 0, 0);
         //画战舰
@@ -44,6 +37,20 @@ public class World extends JPanel {
         }
     }
 
+    private void action() {
+        //创建定时器对象，定义间隔为10毫秒
+        Timer timer = new Timer();
+        int interval = 1000;
+        //匿名内部类new TImeTask(){}
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //TODO 各元素的进场、移动
+                System.out.println(111);
+            }
+        }, interval, interval);
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         //会创建上面的所有对象
@@ -56,5 +63,6 @@ public class World extends JPanel {
         frame.setResizable(false);
         //会自动调用paint()方法
         frame.setVisible(true);
+        world.action();
     }
 }
