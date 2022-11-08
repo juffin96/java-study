c597d44b91973cd6ba6765bc20b58e3951f9ad1e
juffin96 <824293878@qq.com>
2022-11-09 00:05:36 +0800

    2022年11月9日00:04:48

---
+++ web-server-app/src/test/java/com/tedu/webserverapp/WebServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.webserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
